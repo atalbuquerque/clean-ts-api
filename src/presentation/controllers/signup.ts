d29be0ea88cf90387f5a77bfa9f121f835740030
Alexandre <alexandre.teixeira@zup.com.br>
2021-09-27 13:46:10 -0300

    refactor: ensure SignUpController.handle implement correct interdaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,6 @@
+import { HttpResponse, HttpRequest } from '../protocols/http'
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
@@ -11,6 +12,11 @@ export class SignUpController {
       return {
         statusCode: 400,
         body: new Error('Missing param: email')
+      }
+    } else {
+      return {
+        statusCode: 500,
+        body: new Error('Bad request')
       }
     }
   }
