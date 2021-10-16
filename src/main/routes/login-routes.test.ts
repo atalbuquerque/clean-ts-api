ce9864ce15425a77af0594bf3f328b5a93f4b690
Alexandre <alexandre.teixeira@zup.com.br>
2021-10-16 08:35:37 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,29 +2,31 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helper/mongo-helper'
 
-beforeAll(async () => {
-  await MongoHelper.connect(process.env.MONGO_URL)
-})
+describe('Login Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
 
-afterAll(async () => {
-  await MongoHelper.disconnect()
-})
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
 
-beforeEach(async () => {
-  const accountCollection = await MongoHelper.getCollection('accounts')
-  await accountCollection.deleteMany({})
-})
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
 
-describe('SignUp Routes', () => {
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Ale',
-        email: 'ale@gmail.com',
-        password: '123',
-        passwordConfirmation: '123'
-      })
-      .expect(200)
+  describe('POST /signup', () => {
+    test('Should 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Ale',
+          email: 'ale@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
   })
 })
