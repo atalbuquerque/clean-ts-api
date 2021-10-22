7ab77c1a92249cdc3eec0322f350397b6dd3642b
Alexandre <alexandre.teixeira@zup.com.br>
2021-10-22 06:38:39 -0300

    feat: ensure AddSurveyController returns 204 on success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,6 +1,6 @@
 import { HttpRequest, Validation, AddSurvey, AddSurveyModel } from './add-survey-controller-protocols'
 import { AddSurveyController } from './add-survey-controller'
-import { badRequest, serverError } from '../../../helpers/http/http-helper'
+import { badRequest, noContent, serverError } from '../../../helpers/http/http-helper'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -70,10 +70,16 @@ describe('AddSurvey Controller', () => {
     expect(addSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 
-  test('Should returna 500 if AddSurvey throws', async () => {
+  test('Should return 500 if AddSurvey throws', async () => {
     const { sut, addSurveytub } = makeSut()
     jest.spyOn(addSurveytub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 204 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(noContent())
   })
 })
