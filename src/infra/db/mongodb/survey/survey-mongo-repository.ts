29d37ab9f15de72e71877109bfe566d1596c15aa
Alexandre <alexandre.teixeira@zup.com.br>
2021-10-28 18:55:18 -0300

    refactor: organize project files

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,9 +1,9 @@
 import { MongoHelper } from '../helper/mongo-helper'
 import { SurveyModel } from '@/domain/models/survey'
-import { AddSurveyModel } from '@/domain/usecases/add-survey'
+import { AddSurveyModel } from '@/domain/usecases/survey/add-survey'
 import { AddSurveyRepository } from '@/data/protocols/db/survey/add-survey-repository'
 import { LoadSurveyRepository } from '@/data/protocols/db/survey/load-survey-repository'
-import { LoadSurveyByIdRepository } from '@/data/usecases/lod-survey-by-id/db-load-survey-by-id-protocols'
+import { LoadSurveyByIdRepository } from '@/data/usecases/survey/lod-survey-by-id/db-load-survey-by-id-protocols'
 
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveyRepository, LoadSurveyByIdRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
