76355f7131e412273cb19f60f9b6d71b610865b7
Alexandre <alexandre.teixeira@zup.com.br>
2021-11-10 09:31:25 -0300

    refactor: add types to AddSurvey use case

--- src/data/test/mock-db-survey.ts
+++ src/data/test/mock-db-survey.ts
@@ -1,5 +1,4 @@
 import { AddSurveyRepository } from '@/data/protocols/db/survey/add-survey-repository'
-import { AddSurveyParams } from '@/domain/usecases/survey/add-survey'
 import { LoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
 import { LoadSurveyRepository } from '@/data/protocols/db/survey/load-survey-repository'
 import { SurveyModel } from '@/domain/models/survey'
@@ -7,7 +6,7 @@ import { mockSurveyModel, mockSurveyModels } from '@/domain/test'
 
 export const mockAddSurveyRepository = (): AddSurveyRepository => {
   class AddSurveyRespositoryStub implements AddSurveyRepository {
-    async add (surveyData: AddSurveyParams): Promise<void> {
+    async add (surveyData: AddSurveyRepository.Params): Promise<void> {
       return await Promise.resolve()
     }
   }
--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,13 +1,12 @@
 import { MongoHelper, QueryBuilder } from '../helper/'
 import { SurveyModel } from '@/domain/models/survey'
-import { AddSurveyParams } from '@/domain/usecases/survey/add-survey'
 import { AddSurveyRepository } from '@/data/protocols/db/survey/add-survey-repository'
 import { LoadSurveyRepository } from '@/data/protocols/db/survey/load-survey-repository'
 import { LoadSurveyByIdRepository } from '@/data/usecases/survey/lod-survey-by-id/db-load-survey-by-id-protocols'
 import { ObjectId } from 'mongodb'
 
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveyRepository, LoadSurveyByIdRepository {
-  async add (surveyData: AddSurveyParams): Promise<void> {
+  async add (surveyData: AddSurveyRepository.Params): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
   }
