fce449a1c301ca44d2a256797ff6fb872413cb65
Alexandre <alexandre.teixeira@zup.com.br>
2021-11-16 09:10:07 -0300

    refactor: add namespace

--- src/data/usecases/survey/lod-survey-by-id/db-load-survey-by-id.ts
+++ src/data/usecases/survey/lod-survey-by-id/db-load-survey-by-id.ts
@@ -1,10 +1,9 @@
-import { LoadSurveyByIdRepository, SurveyModel, LoadSurveyById } from './db-load-survey-by-id-protocols'
+import { LoadSurveyByIdRepository, LoadSurveyById } from './db-load-survey-by-id-protocols'
 
 export class DbLoadSurveyById implements LoadSurveyById {
   constructor (private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository) {}
 
-  async loadById (id: string): Promise<SurveyModel> {
-    const survey = await this.loadSurveyByIdRepository.loadById(id)
-    return survey
+  async loadById (id: string): Promise<LoadSurveyById.Result> {
+    return await this.loadSurveyByIdRepository.loadById(id)
   }
 }
