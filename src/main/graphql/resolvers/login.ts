b10c51e34d061d83daec7681f82c88db9fd58932
Alexandre <alexandre.teixeira@zup.com.br>
2021-11-25 09:05:16 -0300

    feat: add signup mutation

--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,10 +1,18 @@
 import { adaptResolver } from '@/main/adapters/apollo-server-resolver-adapter'
 import { makeLoginController } from '@/main/factories/controllers/login/login/login-controller-factory'
+import { makeSignUpController } from '@/main/factories/controllers/login/signup/signup-controller-factory'
 
 export default {
   Query: {
     async login (parent: any, args: any) {
       const response = await adaptResolver(makeLoginController(), args)
+      return response.authenticationModel
+    }
+  },
+
+  Mutation: {
+    async signup (parent: any, args: any) {
+      const response = await adaptResolver(makeSignUpController(), args)
       return response.authenticationModel
     }
   }
--- src/main/graphql/type-defs/login.ts
+++ src/main/graphql/type-defs/login.ts
@@ -4,6 +4,10 @@ export default gql`
   extend type Query {
     login (email: String!, password: String!): Account!
   }
+
+  extend type Mutation {
+    signup (name: String!, email: String!, password: String!, passwordConfirmation: String!): Account!
+  }
   
   type Account {
     accessToken: String!
